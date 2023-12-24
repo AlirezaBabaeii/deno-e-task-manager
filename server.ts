858cc6fb422f70776a1c886dc65713fcc2e03a6c
AlirezaBabaei <myalirezababaei@gmail.com>
2023-12-24 20:13:27 +0330

    feat(server): :sparkles: Call initMongooseConnection to connect to the database at initialization time

--- server.ts
+++ server.ts
@@ -1,15 +1,18 @@
 import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import router from "./src/routes/allRoutes.ts";
 import { load } from "https://deno.land/std@0.209.0/dotenv/mod.ts";
+import { initMongooseConnection } from "./src/database/initMongooseConnection.ts";
 const app = new Application();
 
 const env = await load();
 
 const PORT: number | any = env["PORT"] ?? 9191;
 
+initMongooseConnection();
+
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 console.log(`Application is listening on port: ${PORT}`);
 
-await app.listen({ port: PORT });
+app.listen({ port: PORT });
