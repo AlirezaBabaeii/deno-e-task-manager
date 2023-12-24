e05ddd2e2039e2aed767a2b60cc1da0b7e58c7b7
AlirezaBabaei <myalirezababaei@gmail.com>
2023-12-24 20:37:12 +0330

    refactor(middlewares): :sparkles: refactor code with issues #1 for remove Duplicate points, thanks @mikoloism

--- src/middlewares/isAuthorized.ts
+++ src/middlewares/isAuthorized.ts
@@ -1,33 +1,25 @@
+// deno-lint-ignore-file
 import { verify } from "https://deno.land/x/djwt@v2.4/mod.ts";
 import { key } from "../utils/apiKey.ts";
 import { Context } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 export const authourized = async (ctx: Context, next: any) => {
-  try {
-    const headers: Headers = ctx.request.headers;
-    const authorization = headers.get("Authorization");
-    if (!authorization) {
-      ctx.response.status = 401;
-      ctx.response.body = { message: "Authorization failed!"};
-      return;
-    }
-    const jwt = authorization.split(" ")[1];
+  ctx.response.status = 401;
 
-    if (!jwt) {
-      ctx.response.status = 401;
-      return;
-    }
-    const payload = await verify(jwt, key);
+  const headers: Headers = ctx.request.headers;
+  const authorization = headers.get("Authorization");
+  if (!authorization)
+    return (ctx.response.body = {
+      message: 'The "Authorization" Property Not Exists',
+    });
 
-    if (!payload) {
-      throw new Error("!payload");
-    }
-    await next();
-  } catch (error) {
-    ctx.response.status = 401;
-    ctx.response.body = {
-      message: "You are not authorized to access this route",
-    };
-    return;
-  }
+  const jwt = authorization.split(" ")[1];
+  if (!jwt) return (ctx.response.body = { message: "Invalid token" });
+
+  const payload = await verify(jwt, key);
+  if (!payload)
+    return (ctx.response.body = { message: 'Invlaid "payload" in token' });
+
+  ctx.response.status = 200;
+  await next();
 };
